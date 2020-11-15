4eda70af81bfa740b5875caa0b63bbe31c3bd604
ADRIAN-SERGIU TOHĂTAN <adrian.tohatan@stud.ubbcluj.ro>
2020-11-15 15:27:57 +0200

    lab6

--- Models/ExpenseContext.cs
+++ Models/ExpenseContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Tohatan_Adrian_Lab5.Models;
 
 namespace Tohatan_Adrian_Lab5.Models
 {
@@ -13,5 +14,6 @@ namespace Tohatan_Adrian_Lab5.Models
         {
         }
         public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+        public DbSet<Budget> Budget { get; set; }
     }
 }
